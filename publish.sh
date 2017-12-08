41c45d5853c7c26fa37f597a15c5c87d2260818a
Nikos Maris <nmaris@crypteianetworks.com>
2017-12-08 11:20:37 +0200

    publish and test published image only on master

---
+++ publish.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+if [[ $TRAVIS_BRANCH == 'master' && $DOCKER_VERSION == '17.03.0' && $DOCKER_COMPOSE_VERSION == '1.9.0' ]]
+  docker login -u nicosmaris -p "$DOCKER_PASS"
+  docker push $TAG
+
+  docker-compose images
+  docker-compose down
+
+  cat > docker-compose.yml << EOL
+version: '2'
+services:
+  puppeteer:
+    image: $TAG
+    user: $USER
+    cap_add:
+      - SYS_ADMIN
+    shm_size: 512M
+    cpuset: 0-1
+    volumes:
+      - .:/app
+    tty: true
+    stdin_open: true
+EOL
+  docker-compose up -d
+  sleep 3
+  docker-compose images
+  docker-compose exec puppeteer npm start 
+  ls screenshot.png
+fi
+
